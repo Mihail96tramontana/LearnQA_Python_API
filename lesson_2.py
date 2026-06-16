30a4a1975333508d5694105b8a16d78c20aa30d4
Mikhail Liubeznow <znakomets2016@yandex.ru>
2026-06-16 15:30:34 +0300

    Мой третий коммит, обучение по второму уроку прошёл, но к домашке не приступил

--- lesson_2.py
+++ lesson_2.py
@@ -27,10 +27,39 @@ print(response.status_code)
 print()
 
 response = requests.post('https://playground.learnqa.ru/api/get_301', allow_redirects=True)
-first_url = response.history[0]
-print(response.history)
+first_url = response.history[0] # записываем в переменную первый url, на который попадаем
+print(response.history) # выводим промежуточную историю
+print(response.status_code) # выводим финальный статус код
+print(first_url.url) # выводим первый url, на который попадаем
+print(response.url) # выводим финальный url
+print()
+
+payload = {'some_header':'123'}
+response = requests.get('https://playground.learnqa.ru/api/show_all_headers', headers=payload)
+print(response.text) # выводит заголовки отправленные в запросе
+print(response.headers) # выводит заголовки отправленные в ответе сервером обратно
+print()
+
+payload = {'login':'secret_login','password':'secret_pass'}
+response1 = requests.post('https://playground.learnqa.ru/api/get_auth_cookie', data=payload)
+print(response.text)
 print(response.status_code)
-print(first_url.url)
-print(response.url)
+print(dict(response.cookies))
+print(response.headers)
+print()
+
+# отправляем 1-ый запрос с кредами (креды в теле запроса - form-data, метод POST) на сервис (для авторизации)
+payload = {'login':'secret_login','password':'secret_pass8'}
+response_cookies = requests.post('https://playground.learnqa.ru/api/get_auth_cookie', data=payload)
+
+# проверяем, что кука пришла
+if response_cookies.cookies.get('auth_cookie') is not None:
+    response = requests.post('https://playground.learnqa.ru/api/check_auth_cookie', cookies=response_cookies.cookies) # 2-ой запрос. После авторизации проверяем, что cookie действительны и мы авторизованы
+    print(response.text)
+else:
+    print('Cookie не пришло, авторизация не удалась!')
+
+
+
 
 
